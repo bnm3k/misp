e99ca21fbe44d7b8a5a91b78c9a7e6404df700e7
nagamocha3000 <nagamocha@gmail.com>
2020-04-24 16:10:52 +0300

    complete stringfy_val for converting lisp_vals to strings (mostly for printing)

--- src/main.c
+++ src/main.c
@@ -146,80 +146,120 @@ void list_traverse(const Value *l, bool (*fn)(const Value *v)) {
     }
 }
 
-int stringify_val_simple(char *buf, int buf_len, const Value *v) {
-    if (buf_len <= 0) return buf_len - 1;
-    int chars_written;
+/*
+ * stringify_val takes in a lisp val and writes its string representation
+ * into the buffer argument provided. It makes use of snprintf underneath
+ * 
+ * PARAMS:
+ *  - buf       : buffer into which the string representation is written to
+ * 
+ *  - buf_len   : length of buffer, should be greater than 0, since snprintf 
+ *                takes in a size_t, a negative buffer size int ends up being
+ *                treated as a really large positive buf len resulting in mem
+ *                access errors
+ * 
+ *  - v         : the lisp value which's being 'stringified'
+ * 
+ *  - sep       : a separator string that's added between values in lists
+ *                the caller shouldn't be bothered with supplying a string
+ *                here (NULL as an arg suffices) since it's mainly for 
+ *                recursive calls
+ * 
+ * RETURN VALUE:
+ *      if successful, ie, buffer space sufficed, returns a nonnegative (>= 0) value
+ *      equals to the total characters written to the buf.
+ *      if NOT successful, returns a negative number.
+ * 
+ * NOTE:
+ *      since snprintf is used underneath, it's guaranteed that as long as the buffer is
+ *      of size greater than 0, it will always be terminated by '\0' character.
+*/
+int stringify_val(char *buf, int buf_len, const Value *v, const char *sep) {
+    assert(buf_len > 0);
+    if (sep == NULL) sep = "";
+    int total_chars_written;
     switch (v->type) {
     case IS_NIL:
-        chars_written = snprintf(buf, buf_len, "NIL");
+        total_chars_written = snprintf(buf, buf_len, "NIL%s", sep);
         break;
     case IS_INT:
-        chars_written = snprintf(buf, buf_len, "%ld", v->content.integer);
+        total_chars_written = snprintf(buf, buf_len, "%ld%s", v->content.integer, sep);
         break;
     case IS_SYMBOL:
-        chars_written = snprintf(buf, buf_len, "%s", v->content.sym);
+        total_chars_written = snprintf(buf, buf_len, "%s%s", v->content.sym, sep);
         break;
     case IS_ERROR:
-        chars_written = snprintf(buf, buf_len, "%s", v->content.str);
+        total_chars_written = snprintf(buf, buf_len, "%s%s", v->content.str, sep);
         break;
     case IS_EXPR:
     case IS_LIST:
-        chars_written = snprintf(buf, buf_len, "(...)");
+        total_chars_written = snprintf(buf, buf_len, "%s", v->type == IS_EXPR ? "(" : "'(");
+        buf_len = buf_len - total_chars_written;
+        int chars_written = 0;
+        sep = " ";
+        /*
+         * iterate through vals in list/expr and
+         * check that recursive call to stringify was succesful
+         * and that buf_len suffices. Note that after each calls to 
+         * stringify_val, buf_len is updated to the remaining space, 
+         * i.e buf_len = buf_len - chars_written. Furthermore, calls
+         * calls to snprintf are partial(failed ie not all chars written)
+         * if (buf_len <= chars_written) == (buf_len - chars_written <= 0) 
+        */
+        for (const Value *c = list_head(v); c != NIL; c = c->next) {
+            /* dont add space after last val*/
+            if (c->next == NIL) sep = "";
+            if (chars_written < 0 || buf_len <= 0) return -1;
+            chars_written = stringify_val(buf + total_chars_written, buf_len, c, sep);
+            buf_len = buf_len - chars_written;
+            total_chars_written += chars_written;
+        }
+        total_chars_written += snprintf(buf + total_chars_written, buf_len, ")");
+        break;
     }
-    if (buf_len <= 0) {
-        return buf_len - 1;
-    }
-    //if (chars_written < 0) return chars_written;
-    return chars_written;
+    buf_len = buf_len - total_chars_written;
+    if (buf_len <= 0) return -1;
+    return total_chars_written;
 }
 
-int main(void) {
-    // Value *n1 = make_int(1);
-    // Value *n2 = make_int(2);
-    // Value *n3 = make_int(3);
-    // Value *l1 = make_list();
-    // list_push_to_front(l1, n1);
-    // list_push_to_front(l1, n2);
-    // list_push_to_front(l1, n3);
-    // Value *l2 = make_expr();
-    // list_push_to_front(l2, l1);
-    // list_push_to_front(l2, make_sym("+"));
-    // list_push_to_front(l2, make_sym("map"));
-    // print_val(l2);
+void test_stringify_val() {
     char buf[128];
     int buf_len = sizeof(buf);
     int chars_written;
-    Value *n1 = make_int(1);
+
+    Value *n1000 = make_int(1000);
+    chars_written = stringify_val(buf, buf_len, n1000, NULL);
+    assert(chars_written == 4);
+    assert(strcmp(buf, "1000") == 0);
+
     Value *s1 = make_sym("foo_sym");
+    chars_written = stringify_val(buf, buf_len, s1, NULL);
+    assert(chars_written == 7);
+    assert(strcmp(buf, "foo_sym") == 0);
+
     Value *e1 = make_err("bar_err");
+    chars_written = stringify_val(buf, buf_len, e1, NULL);
+    assert(chars_written == 7);
+    assert(strcmp(buf, "bar_err") == 0);
 
-    chars_written = stringify_val_simple(buf, buf_len, n1);
-    printf("buf = %s (len = %d)\n", buf, chars_written);
+    Value *n1 = make_int(1), *n2 = make_int(2), *n3 = make_int(3);
+    Value *l1 = make_list();
+    Value *l2 = make_expr();
 
-    chars_written = stringify_val_simple(buf, buf_len, s1);
-    printf("buf = %s (len = %d)\n", buf, chars_written);
+    list_push_to_front(l1, n1);
+    list_push_to_front(l1, n2);
+    list_push_to_front(l1, n3); // l1 = '(3 2 1)
 
-    chars_written = stringify_val_simple(buf, buf_len, e1);
-    printf("buf = %s (len = %d)\n", buf, chars_written);
-
-    return 0;
+    list_push_to_front(l2, l1);
+    list_push_to_front(l2, make_sym("+"));
+    list_push_to_front(l2, make_sym("map")); // l2 = (map + '(3 2 1))
+    chars_written = stringify_val(buf, buf_len, l2, NULL);
+    assert(chars_written == 16);
+    assert(strcmp(buf, "(map + '(3 2 1))") == 0);
 }
 
-// int print_val(char *buf, int buf_len, const Value *v) {
-//     switch (v->type) {
-
-//     case IS_EXPR:
-//     case IS_LIST:
-//         // printf("%s", v->type == IS_EXPR ? "(" : "'(");
-//         // list_traverse(v, print_list_val);
-//         // printf(")");
-//         printf("LIST");
-//     }
-// }
-
-// bool print_list_val(const Value *v) {
-//     assert(v != NIL);
-//     print_val(v);
-//     if (v->next != NIL) printf(" ");
-//     return true;
-// }
+int main(void) {
+    printf("---------------\n");
+    test_stringify_val();
+    return 0;
+}
