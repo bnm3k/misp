f86001a0f8afec419f1116b356f248f122aab6a4
nagamocha3000 <nagamocha@gmail.com>
2020-04-24 22:34:06 +0300

    add test for stringifying empty lists/exprs

--- test/val_test.c
+++ test/val_test.c
@@ -6,11 +6,18 @@ void test_stringify_val_sufficient_buf_size(void) {
     int buf_len = sizeof(buf);
     int chars_written;
 
+    /* test nil val */
+    chars_written = stringify_val(buf, buf_len, NIL, NULL);
+    TEST_ASSERT_EQUAL_INT(chars_written, 3);
+    TEST_ASSERT_EQUAL_STRING(buf, "NIL");
+
+    /* test numerical val */
     Value *n1000 = make_int(1000);
     chars_written = stringify_val(buf, buf_len, n1000, NULL);
     TEST_ASSERT_EQUAL_INT(chars_written, 4);
     TEST_ASSERT_EQUAL_STRING(buf, "1000");
 
+    /* test symbols and error vals */
     Value *s1 = make_sym("foo_sym");
     chars_written = stringify_val(buf, buf_len, s1, NULL);
     TEST_ASSERT_EQUAL_INT(chars_written, 7);
@@ -24,8 +31,16 @@ void test_stringify_val_sufficient_buf_size(void) {
     Value *n1 = make_int(1), *n2 = make_int(2), *n3 = make_int(3);
     Value *l1 = make_list();
     Value *l2 = make_expr();
-    /* TODO add tests for empty lists and expressions here */
 
+    /* test empty lists and expressions */
+    chars_written = stringify_val(buf, buf_len, l1, NULL);
+    TEST_ASSERT_EQUAL_INT(chars_written, 3);
+    TEST_ASSERT_EQUAL_STRING(buf, "'()");
+    chars_written = stringify_val(buf, buf_len, l2, NULL);
+    TEST_ASSERT_EQUAL_INT(chars_written, 2);
+    TEST_ASSERT_EQUAL_STRING(buf, "()");
+
+    /* test non-empty list */
     list_push_to_front(l1, n1);
     list_push_to_front(l1, n2);
     list_push_to_front(l1, n3);
@@ -33,6 +48,7 @@ void test_stringify_val_sufficient_buf_size(void) {
     TEST_ASSERT_EQUAL_INT(chars_written, 8);
     TEST_ASSERT_EQUAL_STRING(buf, "'(3 2 1)");
 
+    /* test non-empty expr */
     list_push_to_front(l2, l1);
     list_push_to_front(l2, make_sym("+"));
     list_push_to_front(l2, make_sym("map"));
